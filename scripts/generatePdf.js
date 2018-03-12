cb3ad7fc819c650c6ba32bac5688dd7167a237ce
Javier Guerrero <jguerrero@wayfair.com>
2018-03-11 20:12:06 -0400

    Update PDF exporter script to catch pages with missing image parts

--- scripts/generatePdf.js
+++ scripts/generatePdf.js
@@ -10,29 +10,66 @@ const PAGE = {
 
 fs.readdir(INPUT, (err, files) => {
   const exportsMap = {};
-   const images = files.filter(file => {
-    if (/page\d+-part[1-2].pdf/.test(file)) {
-      return true;
-    } else if (file.includes('pdf')) {
-      console.log(`Check ${file} it doesn't follow the page#-part# format!`);
-      return false;
-    }
-    return false;
-   })
-   console.log("Convertion has started...");
-   images.forEach((image) => {
-    if (image.includes('pdf')) {
+   const {convertableFiles, missingPairFiles} = verifyFiles(files);
+   // Convert files that have matching pairs.
+   convertableFiles.forEach((image) => {
       createImage(image).then(createdImage => {
+        // Keep track of the converted images
         exportsMap[createdImage] = true;
-
         const {background, overlay} = getImageParts(createdImage);
+        // Only overlay images when both background and overlay have been converetd. 
         if (exportsMap[background] && exportsMap[overlay]) {
           overlayImages(background, overlay).then(convertToPdf);
         }
       });
-    }
    });
+   if (missingPairFiles.length) {
+    console.log("Some files have missing matching pairs. Take a look at: ", missingPairFiles.join("\n "));
+   }
 });
+
+/*
+  Verifies the formatting of files and separates the files into arrays of matching or missing pairs.
+*/
+function verifyFiles(files) {
+  const filesMap = {};
+  const knownPages = {};
+  // Only want files that match the formatting and are unique.
+  const validFiles = files.filter((file) => {
+    if (satisfyFormat(file) && !filesMap[file]) {
+      const pageNumber = file.match(/page\d+/)[0];
+      knownPages[pageNumber] = !knownPages[pageNumber] ? true : false;
+      return true;
+    }
+    return false;
+  });
+  const returnValue = {
+    convertableFiles: [],
+    missingPairFiles: []
+  };
+  // Only want the files for pages with matching pairs.
+  return validFiles.reduce((acc, file) => {
+    const matchingFilename = getNameOfMatchingFile(file);
+    const fileHasMatch = validFiles.some((possibleMatch) => possibleMatch === matchingFilename);
+    // Place file in array of matching pairs or array of missing pairs.
+    if (fileHasMatch) {
+      acc.convertableFiles.push(file);
+    } else {
+      acc.missingPairFiles.push(file);
+    }
+    return acc;
+  }, returnValue);
+}
+
+// PDF files need to satisfy this filenaming format to be convertable.
+function satisfyFormat(file) {
+  return /^page\d+-part[1-2]+.pdf$/.test(file);
+}
+
+// Draws the name of the matching file given the passed filename.
+function getNameOfMatchingFile(file) {
+  return file.includes('part1') ? file.replace('part1', 'part2') : file.replace('part2', 'part1');
+}
 
 function convertToPdf(image) {
   return (new Promise((res, rej) => {
@@ -50,6 +87,20 @@ function convertToPdf(image) {
   }));
 }
 
+// Will merge all the page pdfs into one.
+function mergePdfs(pdfFiles) {
+  return (new Promise((res, rej) => {
+    const command = `convert -density 300 ${pdfFiles.join( )} ${OUTPUT}/output.pdf`;
+    exec(command, (err) => {
+      if (!err) {
+        res();
+      } else {
+        rej();
+      }
+    });
+  }));
+}
+
 function deleteFile(path) {
   return (new Promise((res, rej) => {
     const command = `rm ${path}`;
@@ -63,9 +114,20 @@ function deleteFile(path) {
   }));
 }
 
+// Returns the filename of the background and overlay images.
 function getImageParts(createdImage) {
-    const background = createdImage.includes('part1') ? createdImage : createdImage.replace('part2', 'part1');
-    const overlay = background.replace('part1', 'part2');
+
+    let background = '';
+    let overlay = '';
+
+    if (createdImage.includes('part1')) {
+      background = createdImage;
+      overlay = getNameOfMatchingFile(createdImage);
+    } else {
+      background = getNameOfMatchingFile(createdImage);
+      overlay = createdImage;
+    }
+
     return {background, overlay};
 }
 
